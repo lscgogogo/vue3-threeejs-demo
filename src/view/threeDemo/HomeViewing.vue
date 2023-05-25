a9bedd0f7ddd12234345f4f94ed46f23af6ee279
DESKTOP-0FDNEV4\yc <850925770@qq.com>
2023-05-25 16:59:40 +0800

    chore:配置vue3自动引入，@路径配置

---
+++ src/view/threeDemo/HomeViewing.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page">
+    <div id="webgl" ref="container" class="webgl"></div>
+    <div class="map">
+      <div ref="tagDiv" class="tag"></div>
+      <img src="@/assets/img/open/homeViewing/map.gif" alt="" />
+    </div>
+    <div v-if="progress !== 100" class="loading"></div>
+    <div v-if="progress !== 100" class="progress">
+      <span>新房加载中：{{ progress }}%</span>
+    </div>
+    <div class="title">VR全景看房</div>
+  </div>
+</template>
+
+<script setup>
+import * as THREE from 'three'
+
+
+const container = ref
+console.log('container', container)
+</script>
+
+<style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path';
+//引入自动引入插件
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), AutoImport({
+    imports: ['vue'], dts: 'src/auto-import.d.ts'
+  })],
+  // 配置 @ 符号
+  resolve: {
+    alias: {
+      "@": path.resolve("./src")
+    }
+  }
 })
